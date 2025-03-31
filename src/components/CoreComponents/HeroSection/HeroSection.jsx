74c6271181d7b43be12aaa50333b75148d4686fc
Mohammed Aboutalha <mohammedaboutalha2023@gmail.com>
2025-03-31 14:24:30 +0100

    drp the background color

--- src/components/CoreComponents/HeroSection/HeroSection.jsx
+++ src/components/CoreComponents/HeroSection/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = ({ children, illustration }) => {
       <div
         className={styles.headerBloc}
         style={{
-          background: `url(${im}) -254px -67px no-repeat, url(${im}) 100% 100% no-repeat, linear-gradient(90deg,rgba(217,235,232,1) 100%,rgba(236,246,244,1) 72%,rgba(242,242,242,1) 74%, rgba(217,235,232,1) 77%,rgba(246,255,254,1) 100%)`,
+          background: `url(${im}) -254px -67px no-repeat, url(${im}) 100% 100% no-repeat, linear-gradient(90deg,rgb(255, 255, 255) 100%,rgb(255, 255, 255) 72%,rgba(242,242,242,1) 74%, rgba(217,235,232,1) 77%,rgba(246,255,254,1) 100%)`,
         }}
       >
         <div className={styles.pageDescription}>{children}</div>
