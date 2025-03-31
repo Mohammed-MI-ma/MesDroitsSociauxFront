8b71516d4602b9f3c84dba3c51d78ee2666fa41c
Mohammed Aboutalha <mohammedaboutalha2023@gmail.com>
2025-03-31 12:48:14 +0100

    hide the loginbutton

--- src/components/CoreComponents/LoginButton/LoginButton.jsx
+++ src/components/CoreComponents/LoginButton/LoginButton.jsx
@@ -31,32 +31,35 @@ const LoginButton = () => {
   const handleOpenChange = (newOpen) => {
     setOpen(newOpen); // Toggle popover visibility
   };
-
   return (
-    <Popover
-      color="var(--color-primary)"
-      content={
-        <div style={{ maxWidth: "400px", color: "white" }}>
-          {t("tooltip.content")}
+    <>
+      {location !== "maroc-connect/connexion" && (
+        <Popover
+          color="var(--color-primary)"
+          content={
+            <div style={{ maxWidth: "400px", color: "white" }}>
+              {t("tooltip.content")}
 
-          <a onClick={hide}> {t("tooltip.Fermer")}</a>
-        </div>
-      }
-      title={<h1 style={{ color: "white" }}>{t("tooltip.title")}</h1>}
-      trigger="click"
-      open={open} // Controlled open state
-      onOpenChange={handleOpenChange} // Handle opening and closing
-      getPopupContainer={(triggerNode) => triggerNode.parentNode} // Ensure popover is within the same container
-    >
-      <Link to={"/maroc-connect/connexion"}>
-        <Button className={style.loginButton} type="text">
-          <RiLoginCircleFill style={{ color: "var(--color-primary)" }} />
-          <div>
-            <p>{t("Seconnecter")}</p>
-          </div>
-        </Button>{" "}
-      </Link>
-    </Popover>
+              <a onClick={hide}> {t("tooltip.Fermer")}</a>
+            </div>
+          }
+          title={<h1 style={{ color: "white" }}>{t("tooltip.title")}</h1>}
+          trigger="click"
+          open={open} // Controlled open state
+          onOpenChange={handleOpenChange} // Handle opening and closing
+          getPopupContainer={(triggerNode) => triggerNode.parentNode} // Ensure popover is within the same container
+        >
+          <Link to={"/maroc-connect/connexion"}>
+            <Button className={style.loginButton} type="text">
+              <RiLoginCircleFill style={{ color: "var(--color-primary)" }} />
+              <div>
+                <p>{t("Seconnecter")}</p>
+              </div>
+            </Button>{" "}
+          </Link>
+        </Popover>
+      )}
+    </>
   );
 };
 
